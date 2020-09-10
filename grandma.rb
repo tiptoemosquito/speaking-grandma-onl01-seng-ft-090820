53198abc750f13d1d76619b62727c31f33774b91
Christina Howard <clhoward33@icloud.com>
2020-09-10 02:01:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-def speak_to_grandma = (speak)
+def speak_to_grandma 
 elsif puts "HUH?! SPEAK UP, SONNY!"
 elsif puts "NO, NOT SINCE 1938!"
 elsif puts "I LOVE YOU GRANDMA!"
