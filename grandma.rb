e392109434266f9fe075db4d5a57ef947c207957
Christina Howard <clhoward33@icloud.com>
2020-09-10 02:23:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 def speak_to_grandma (phrase)
 
 if phrase == phrase.upcase
-  return "NO, NOT SINCE 1938!"
+  return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == "I LOVE YOU GRANDMA!"
 	return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase != phrase.upcase
-	return "HUH?! SPEAK UP, SONNY!"
+  return "NO, NOT SINCE 1938!"
 end
 end
